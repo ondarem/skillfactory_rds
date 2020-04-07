116cfeef43bdc41ed2869f32a8ab33e953bec2b8
unknown <ondarem@yandex.ru>
2020-04-07 21:06:54 +0300

    Added my final version of the task Guess the number

--- module_0/sf_module_0/main.py
+++ module_0/sf_module_0/main.py
@@ -1,13 +1,54 @@
 import numpy as np
-count = 0                            # счетчик попыток
-number = np.random.randint(1,100)    # загадали число
-print ("Загадано число от 1 до 99")
+#Первый вариант
+def score_game(game_core):
+  count_ls = []
+  np.random.seed(1) 
+  random_array = np.random.randint(1,101, size=(1000))
+  for number in random_array:
+    count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+  print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+  return(score)
 
-while True:                        # бесконечный цикл
-    predict = int(input())         # предполагаемое число
-    count += 1                     # плюсуем попытку
-    if number == predict: break    # выход из цикла, если угадали
-    elif number > predict: print (f"Угадываемое число больше {predict} ")
-    elif number < predict: print (f"Угадываемое число меньше {predict} ")
-        
-print (f"Вы угадали число {number} за {count} попыток.")
+def game_core_oem1(number):
+  count = 1
+  min_n = 1
+  max_n = 101
+  predict = np.random.randint(min_n,max_n)
+  while number != predict:
+    count += 1
+    if number > predict:
+      min_n = predict
+    elif number < predict:
+      max_n = predict
+    predict = np.random.randint(min_n,max_n)
+  return(count)
+
+score_game(game_core_oem1)
+
+#Второй вариант
+def score_game1(game_core1):
+  count_ls = []
+  np.random.seed(1) 
+  random_array = np.random.randint(1,101, size=(1000))
+  for number in random_array:
+    count_ls.append(game_core1(number))
+    score = int(np.mean(count_ls))
+  print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+  return(score)
+
+def game_core_oem2(number):
+  count = 1
+  min_n = 1
+  max_n = 101
+  predict = int(max_n/2)
+  while number != predict:
+    count += 1
+    if number > predict:
+      min_n = predict
+    elif number < predict:
+      max_n = predict
+    predict = int(max_n-((max_n-min_n)/2))
+  return(count)
+
+score_game1(game_core_oem2)
